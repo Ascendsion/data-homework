0335c15298f3e3a8bc75feba4050309442ad01a5
Alexander Olvera <96093396+Ascendsion@users.noreply.github.com>
2022-05-06 23:08:36 -0500

    Add files via upload

---
+++ Lesson 5 Hands-on Rstudio.R
@@ -0,0 +1,13 @@
+rr = data.frame(rivers)
+
+head(rr)
+
+r.histo <- ggplot(rr, aes(x = rivers))
+r.histo + geom_histogram(binwidth =30) + ggtitle("Histogram of Rivers") + xlab("Length (in miles)")
+
+r.boxp <- ggplot(rr, aes(x=" ", y = rivers))
+r.boxp + geom_boxplot() + xlab(" ")
+
+ggplot(rr, aes(sample = rivers)) + geom_qq()
+
+summary(rr$rivers)
