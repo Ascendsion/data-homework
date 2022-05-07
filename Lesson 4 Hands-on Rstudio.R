702d80e41fae368a68212ba461ecadbc1830765d
Alexander Olvera <96093396+Ascendsion@users.noreply.github.com>
2022-05-06 23:07:27 -0500

    Add files via upload

---
+++ Lesson 4 Hands-on Rstudio.R
@@ -0,0 +1,17 @@
+library(datasets)
+
+faithful
+
+eruptions.times <- faithful$eruptions
+
+short <- eruptions.times[eruptions.times <= 3]
+  
+long <- eruptions.times[eruptions.times > 3]
+
+mean(short)
+
+mean(long)
+
+sd(short)
+
+sd(long)
