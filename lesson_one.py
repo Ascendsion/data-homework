3ceee5a081fcd79e4428631f89d924146a1d7629
Alexander Olvera <96093396+aolvera45@users.noreply.github.com>
2022-06-08 23:26:31 -0500

    Add files via upload

---
+++ lesson_one.py
@@ -0,0 +1,22 @@
+question_one = "What is your name?"
+question_two = "How old are you?"
+question_three = "How many months is that?"
+
+answer_one = "My name is \"Billy\"."
+answer_two = "30"
+answer_three = "360"
+
+print(question_one)
+print(answer_one)
+print(question_two)
+print(answer_two)
+print(question_three)
+print(answer_three)
+
+my_string = "This is a string example."
+my_integer = 12345
+my_float = 123.45
+
+print(my_string)
+print(my_integer)
+print(my_float)
